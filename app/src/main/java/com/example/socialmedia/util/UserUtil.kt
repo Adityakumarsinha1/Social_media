02d8ce6edd3d68d23ea8576e25d85cae2f06f324
Adityakumarsinha1 <adityakumarsinha0000@gmail.com>
2023-01-09 23:36:27 +0530

    working of searching feature

--- app/src/main/java/com/example/socialmedia/util/UserUtil.kt
+++ app/src/main/java/com/example/socialmedia/util/UserUtil.kt
@@ -1,21 +1,27 @@
 package com.example.socialmedia.util
 
+import android.util.Log
+import android.widget.Toast
+import com.example.socialmedia.firebaseUser
+import com.example.socialmedia.model.Users
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.auth.User
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.FirebaseDatabase.*
+import com.google.firebase.database.ktx.getValue
 
-class UserUtil {
-
-    var user: User? = null
-
+object UserUtil {
+    var user: Users? = null
+    private lateinit var database: FirebaseDatabase
     fun getCurrentUser() {
         if (FirebaseAuth.getInstance().currentUser != null) {
-            FirebaseFirestore.getInstance().collection("Users")
-                .document(FirebaseAuth.getInstance().currentUser?.uid!!)
-                .get().addOnCompleteListener {
-                    user = it.result?.toObject(User::class.java)
-
-                }
+            database = FirebaseDatabase.getInstance("https://socialmedia-e0647-default-rtdb.asia-southeast1.firebasedatabase.app")
+            database.reference.child("Users").child(firebaseUser.uid)
+                .get().
+            addOnSuccessListener {
+                user = it.getValue(Users::class.java)
+                Log.d("@@@@","user=${user.toString()}")
+            }
         }
     }
 }
